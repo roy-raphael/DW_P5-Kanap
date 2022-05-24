3c96e06ce477c1b69d5dec0b656d161ab6ccce25
Raphael Roy <roy.raphael.33@gmail.com>
2022-05-24 14:52:56 +0200

    feat : add product data (and options) to the product page

--- front/js/product.js
+++ front/js/product.js
@@ -15,7 +15,24 @@ async function getProduct() {
 
 async function displayProduct() {
     let productJson = await getProduct();
-    // TODO
+    // Image
+    let imageItem = document.createElement("img");
+    imageItem.setAttribute("src", productJson.imageUrl);
+    imageItem.setAttribute("alt", productJson.altTxt);
+    document.getElementsByClassName("item__img")[0].appendChild(imageItem);
+    // Contenu
+    document.getElementById("title").textContent = productJson.name;
+    document.getElementById("price").textContent = (productJson.price/100).toLocaleString();
+    // Description
+    document.getElementById("description").textContent = productJson.description;
+    // Options
+    let colorsSelection = document.getElementById("colors");
+    for (let color of productJson.colors) {
+        let colorOption = document.createElement("option");
+        colorOption.setAttribute("value", color);
+        colorOption.textContent = color;
+        colorsSelection.appendChild(colorOption);
+    }
 }
 
 displayProduct();
